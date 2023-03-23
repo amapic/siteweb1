f56d480c314c0568dfa0065708c41ae816e54770
amapic <amaury.pichat@gmail.com>
2023-03-23 18:11:01 +0100

    bien avncé

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,55 +2,187 @@ import React, { useEffect, useRef, useLayoutEffect, useState } from "react";
 import gsap, { Power3 } from "gsap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import ParralaxBackground from "../siteWeb1/ParralaxBackground";
+import { LoremIpsum } from "lorem-ipsum";
+
+import ParralaxBackground, {
+  TimelinePart2,
+} from "../siteWeb1/ParralaxBackground";
+import Part2 from "../siteWeb1/Part2";
 const Home = () => {
-  const root = useRef(null);
+  const mouse = useRef(null);
 
   const [state, setState] = useState(false);
 
-  useEffect(() => {
-    // console.log("useEffect() call", "(React 18 strict mode calls twice!)");
+  const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4,
+    },
+    wordsPerSentence: {
+      max: 16,
+      min: 4,
+    },
   });
 
+  useLayoutEffect(() => {
+    var cursorWidth = 40;
+
+    if (document) {
+      TimelinePart2();
+
+      const setX = gsap.quickTo("#mousemove", "x", {
+        duration: 0.5,
+        ease: "power2",
+      });
+
+      const setY = gsap.quickTo("#mousemove", "y", {
+        duration: 0.5,
+        ease: "power2",
+      });
+
+      // const ChangeeEnter = gsap.to(".aa", {
+      //   duration: 0.5,
+      //   scale: 2,
+      //   ease: "power2",
+      // });
+
+      const ChangeeOut = gsap.to(".aa", {
+        duration: 0.5,
+        scale: 1,
+        ease: "power2",
+      });
+
+      document.addEventListener("pointermove", (e) => {
+        setX(e.clientX - cursorWidth / 2);
+        setY(e.clientY - cursorWidth / 2);
+      });
+
+      document.getElementById("menu").addEventListener("pointerenter", (e) => {
+        // console.log(e.target)
+        mouse.current.classList.add("aa");
+      });
+
+      document.getElementById("menu").addEventListener("pointerout", (e) => {
+        // console.log(e)
+        mouse.current.classList.remove("aa");
+      });
+    }
+  }, []);
 
   return (
     <>
       <ParralaxBackground />
-      <div>
+      <div ref={mouse} id="mousemove"></div>
+      <div className="overflow-hidden">
         <header
           id="header"
           className="shadow bg-white w-100 h-15 justify-content-between position-fixed top-0 left-0 d-flex flex-row"
         >
           <img className="ps-4 m-2" src="title.svg" />
           <nav className="w-20 h-100 d-flex flex-row justify-content-center align-items-center">
-            <div className="w-50 h-100 d-flex justify-content-center align-items-center">
+            <div
+              id="contact"
+              className="w-50 h-100 d-flex justify-content-center align-items-center"
+            >
               Contact
             </div>
-            <div className="bg-orange w-50 h-100 d-flex justify-content-center align-items-center">
+            <div
+              id="menu"
+              className="bg-brown w-50 h-100 d-flex justify-content-center align-items-center"
+            >
               Menu
             </div>
           </nav>
         </header>
-        <div id="content">
-          <div className="container mx-auto d-flex flex-row">
-            <div
-            className="d-flex flex-row row"
-            >
-              <div className="col col-8">
-                <h4>Agence web à Lyon</h4>
-                <p className="title">
-                  Spécialistes en développement de solutions digitales sur
-                  mesure
-                </p>
-                Création de site Internet, développement d'outils digitaux et
-                d'applications mobiles, nous accompagnons les professionnels qui
-                désirent gagner en visibilité, en productivité, et en
-                efficacité. Découvrez nos services
+        <div id="content" className="parralax">
+          <div className="part1">
+            <div className="container mx-auto d-flex flex-column">
+              <div className="d-flex flex-row row">
+                <div className="col col-8">
+                  <h4>{lorem.generateWords(4)}</h4>
+                  <p className="title">
+                    {/* Spécialistes en développement de solutions digitales sur
+                    mesure */}
+                    {lorem.generateWords(7)}
+                  </p>
+                  {/* Création de site Internet, développement d'outils digitaux et
+                  d'applications mobiles, nous accompagnons les professionnels
+                  qui désirent gagner en visibilité, en productivité, et en
+                  efficacité. Découvrez nos services */}
+                  {lorem.generateParagraphs(1)}
+                </div>
+                <div className="col col-4 mx-auto d-none d-lg-block">
+                  <img className="h-75" src="/siteWeb1/main.svg" />
+                </div>
               </div>
-              <div className="col col-4 mx-auto d-none d-lg-block">
-                <img 
-                className="h-75"
-                src="/siteWeb1/main.svg"/>
+            </div>
+          </div>
+          <div className="part2 bg-grey position-relative">
+            <div className="jauge w-5 h-100 my-2 position-absolute flex-column">
+              <div className="bg-yellow p-1 m-1 row rounded h-25 w-100"></div>
+              <div className="bg-yellow p-1 m-1  rounded row h-25 w-100"></div>
+              <div className="bg-yellow p-1 m-1  rounded row h-25 w-100"></div>
+              <div className="bg-yellow p-1 m-1  rounded row h-25 w-100"></div>
+            </div>
+            <div className="container w-100 mx-auto d-flex flex-column">
+              <div className="row">
+                <h1 className="ps-4">{lorem.generateWords(4)}</h1>
+              </div>
+              <div className="row">
+                <div className="col-12 col-lg-6 card1 cardd">
+                  <div className="shadow p-3 m-2 bg-white rounded w-100 h-100">
+                    <div className="d-flex flex-row w-100">
+                      <img
+                        className="imglogo mt-2"
+                        src="siteWeb1/logo/budget.png"
+                      />
+
+                      <h2 className="ps-3 mb-4">{lorem.generateWords(1)}</h2>
+                    </div>
+
+                    <h6>{lorem.generateSentences(1)}</h6>
+                  </div>
+                </div>
+                <div className=" col-12 col-lg-6 card2 cardd">
+                  <div className="shadow p-3 m-2 bg-white rounded w-100 h-100">
+                    <div className="d-flex flex-row w-100">
+                      <img
+                        className="imglogo mt-2"
+                        src="siteWeb1/logo/report.png"
+                      />
+
+                      <h2 className="ps-3 mb-4">{lorem.generateWords(1)}</h2>
+                    </div>
+
+                    <h6>{lorem.generateSentences(2)}</h6>
+                  </div>
+                </div>
+                <div className=" col-12 col-lg-6 card3 cardd">
+                  <div className="shadow p-3 m-2 bg-white rounded w-100 h-100">
+                    <div className="d-flex flex-row w-100">
+                      <img
+                        className="imglogo mt-2"
+                        src="siteWeb1/logo/business.png"
+                      />
+
+                      <h2 className="ps-3 mb-4">{lorem.generateWords(1)}</h2>
+                    </div>
+                    <h6>{lorem.generateSentences(4)}</h6>
+                  </div>
+                </div>
+                <div className=" col-12 col-lg-6 card4 cardd">
+                  <div className="shadow p-3 m-2 bg-white rounded w-100 h-100">
+                    <div className="d-flex flex-row w-100">
+                      <img
+                        className="imglogo mt-2"
+                        src="siteWeb1/logo/analytics.png"
+                      />
+
+                      <h2 className="ps-3 mb-4">{lorem.generateWords(1)}</h2>
+                    </div>
+                    <h6>{lorem.generateSentences(4)}</h6>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- siteWeb1/ParralaxBackground.jsx
+++ siteWeb1/ParralaxBackground.jsx
@@ -1,12 +1,9 @@
 import { useEffect, useRef } from "react";
-import {gsap} from "gsap";
+import { gsap } from "gsap";
 
-import { ScrollTrigger} from "gsap/dist/ScrollTrigger";
-// import { ScrollSmoother } from "gsap-trial/dist/ScrollSmoother";
-// import ScrollSmoother from "gsap/dist/ScrollSmoother";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-// gsap.registerPlugin(ScrollSmoother);
 
 export default function ParralaxBackground() {
   const ref = useRef(null);
@@ -15,20 +12,17 @@ export default function ParralaxBackground() {
 
   useEffect(() => {
     let ctx = gsap.context(() => {
-
       const tl = gsap.timeline({
         scrollTrigger: {
-          trigger: ".container",
+          trigger: ".parralax",
           start: "top top",
           end: "bottom top",
           scrub: true,
-          // markers:true
-        }
+          markers:true
+        },
       });
 
-
       gsap.utils.toArray(".kibouge").forEach((layer) => {
-
         const speed = layer.dataset.speed;
         const movement = -(layer.offsetHeight * speed);
         tl.to(layer, { y: movement, ease: "none" }, 0);
@@ -37,46 +31,214 @@ export default function ParralaxBackground() {
 
     return () => ctx.revert();
   });
+
   return (
     <>
       <img
         ref={ref}
         src="/siteWeb1/brownCircle.png"
-        height={"100px"}
-        width={"100px"}
+        height={"50px"}
+        width={"50px"}
         className="position-fixed kibouge"
         style={{
-          transform: "rotate(45deg)",
+          transform: "scale(2)",
           left: "10vh",
+          top:"15vh",
+          zIndex:"100"
         }}
-        data-speed="0.10"
+        data-speed="0.6"
       />
       <img
         ref={ref2}
         src="/siteWeb1/brownSquare.png"
-        height={"100px"}
-        width={"100px"}
+        height={"50px"}
+        width={"50px"}
         className="position-fixed kibouge"
-        data-speed="0.20"
+        data-speed="0.6"
         style={{
-          transform: "rotate(45deg)",
+          transform: "rotate(45deg) scale(2)",
           right: "10vh",
           top: "20vh",
         }}
       />
+      {/* <img
+        ref={ref}
+        src="/siteWeb1/brownCircle.png"
+        height={"50px"}
+        width={"50px"}
+        className="position-fixed kibouge"
+        style={{
+          transform: "scale(0.5)",
+          right: "20vh",
+          top:"15vh",
+          zIndex:"100"
+        }}
+        data-speed="0.6"
+      /> */}
       <img
         ref={ref3}
         src="/siteWeb1/darkBrownSquare.png"
-        height={"100px"}
-        width={"100px"}
+        height={"50px"}
+        width={"50px"}
         className="position-fixed kibouge"
-        data-speed="0.50"
+        data-speed="0.9"
         style={{
-          transform: "rotate(5deg)",
-          right: "10vh",
+          transform: "rotate(5deg) scale(3)",
+          right: "-5vh",
           bottom: "20vh",
+        }}
+      />
+      <img
+        // ref={ref3}
+        src="/siteWeb1/darkBrownSquare.png"
+        height={"50px"}
+        width={"50px"}
+        className="position-fixed kibouge"
+        data-speed="0.3"
+        style={{
+          transform: "rotate(5deg) scale(1)",
+          right: "50vh",
+          top: "10vh",
+          zIndex:"100"
         }}
       />
     </>
   );
 }
+
+export function TimelinePart2() {
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".card1",
+      start: "top bottom",
+      end: "bottom bottom",
+      once: true,
+    },
+  });
+
+  const tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".card2",
+      start: "top bottom",
+      end: "bottom bottom",
+      once: true,
+    },
+  });
+
+  const tl3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".card3",
+      start: "top bottom",
+      end: "bottom bottom",
+      once: true,
+    },
+  });
+
+  const tl4 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".card4",
+      start: "top bottom",
+      end: "bottom bottom",
+      once: true,
+    },
+  });
+
+  const tlJauge = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".jauge",
+      start: "bottom bottom",
+      end: "bottom bottom",
+      once: true,
+      // markers:true
+    },
+  });
+
+  tlJauge.fromTo(".jauge div:nth-child(1)", {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    // backgroundColor:"red",
+    duration:"0.5"
+  },">0.5");
+
+  tlJauge.fromTo(".jauge div:nth-child(2)", {
+    opacity: 0,
+  },{
+    opacity: 1,
+    backgroundColor:"red",
+    duration:"1"
+  },">0.5");
+
+  tlJauge.fromTo(".jauge div:nth-child(3)",{
+    opacity: 0,
+  }, {
+    opacity: 1,
+    backgroundColor:"red",
+    duration:"3"
+  },">0.5");
+
+  tlJauge.fromTo(".jauge div:nth-child(4)",{
+    opacity: 0,
+  }, {
+    opacity: 1,
+    backgroundColor:"red"
+  },">0.5");
+
+  tl.fromTo(
+    ".card1 div",
+    {
+      opacity: 0,
+      y: "150px",
+      duration:"3"
+    },
+    {
+      opacity: 1,
+      y: "0px",
+    }
+
+    // "0.4"
+  );
+
+  tl2.fromTo(
+    ".card2 div",
+    {
+      opacity: 0,
+      y: "150px",
+      duration:"3"
+    },
+    {
+      opacity: 1,
+      y: "0px",
+    }
+    // "0.5"
+  );
+
+  tl3.fromTo(
+    ".card3 div",
+    {
+      opacity: 0,
+      y: "150px",
+    },
+    {
+      opacity: 1,
+      y: "0px",
+    }
+    // "0.6"
+  );
+
+  tl4.fromTo(
+    ".card4 div",
+    {
+      opacity: 0,
+      y: "150px",
+    },
+    {
+      opacity: 1,
+      y: "0px",
+    }
+    // "0.7"
+  );
+}
+// }
