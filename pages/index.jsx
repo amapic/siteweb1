4d6b9aef3ded309f92d2446aa8b1c59e0873d499
amapic <amaury.pichat@gmail.com>
2023-12-06 10:00:54 +0100

    change ' to " in gtag manager

--- pages/index.jsx
+++ pages/index.jsx
@@ -70,9 +70,9 @@ const Home = () => {
     <Script >
         window.dataLayer = window.dataLayer || [];
         function gtag() { dataLayer.push(arguments) }
-        gtag('js', new Date());
+        gtag("js", new Date());
 
-        gtag('config', 'G-LTCXHF5790');
+        gtag("config", "G-LTCXHF5790");
 
     </Script>
        
