31accdd150a28e7ccc0212d531c865ba924bc599
amapic <amaury.pichat@gmail.com>
2023-12-10 17:22:47 +0100

    enlevage code google analytucs

--- pages/index.jsx
+++ pages/index.jsx
@@ -65,16 +65,16 @@ const Home = () => {
         <link rel="shortcut icon" href="/siteWeb1/favicon.ico" />
         
       </Head>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-LTCXHF5790"></Script>
-        <Script id="google-analytics">
+      {/* <Script src="https://www.googletagmanager.com/gtag/js?id=G-LTCXHF5790"></Script> */}
+        {/* <Script id="google-analytics">
           {`
         window.dataLayer = window.dataLayer || [];
         function gtag() { dataLayer.push(arguments) }
-        gtag(&apos;js&apos;, new Date());
+        gtag(js, new Date());
 
-        gtag(&apos;config&apos;, &apos;G-LTCXHF5790&apos;);
+        gtag(config, G-LTCXHF5790);
         `}
-        </Script>
+        </Script> */}
       {/* <Navv /> */}
       {/* <Carousel /> */}
       <div ref={mouse} id="mousemove">
